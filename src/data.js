cb5ef24abd813da5127c81f9ca03f8acf50cf3d1
Tu-username <tucorreo@gmail.com>
2018-12-16 22:06:16 -0500

    Estructura del HTML, pagina de INICIO

--- src/data.js
+++ src/data.js
@@ -1,8 +1,18 @@
-// esta es una función de ejemplo
-// puedes ver como agregamos la función a nuestro objeto global window
+let arrpokemon = POKEMON.pokemon
+//for (let i = 0; i < arrpokemon.length; i++){
+  //console.log(arrpokemon[i].name);}
+console.log (arrpokemon.sort(function (a,b){
+    if (a.name > b.name) {
+      return 1;
+    }
 
-const example = () => {
-  return 'example';
-};
+    if (a.name < b.name) {
+      return -1;
+    }
+  
+}));
 
-window.example = example;
+let apokemon = Object.values(POKEMON.pokemon )
+for (let i = 0; i < apokemon.length; i++){
+  const abc= apokemon [i]
+console.log (abc.name);}
