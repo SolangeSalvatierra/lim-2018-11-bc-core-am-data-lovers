efc4f15fbbff685311ba2cdcfb777354d51434b1
SolangeCorySalvatierraQuispe <solangraf@gmail.com>
2018-12-18 15:58:18 -0500

    Data mostrada en HTM

--- src/data.js
+++ src/data.js
@@ -1,18 +1,44 @@
-let listpokemon = POKEMON.pokemon;
+/*let listpokemon = POKEMON.pokemon;
 let pokemon = JSON.stringify (listpokemon);
-document.getElementById ("list-pokemones").innerHTML = pokemon;
+document.getElementById ("list-pokemones").innerHTML = pokemon;*/
 
 
+// esta es una función de ejemplo
+// puedes ver como agregamos la función a nuestro objeto global window
 
-/*let listpokemon = POKEMON.pokemon
+/*const example = () => {
+  return 'example';
+};
+
+window.example = example;*/
+
+let listpokemon = POKEMON.pokemon
+let pokemones = [];
 for (let i = 0; i < listpokemon.length; i++) {
- console.log(listpokemon[i].id);
+  pokemones.push(listpokemon[i].id);
 }
 
-document.getElementById("list-pokemones").innerHTML = listpokemon;*/
-
-
-
+//Funcion para pintar tarjetas con datos pokemon
+const crearTemplateDeCard = (data) => {
+  let templateListOfCards = '';
+  // recorremos nuestro array con forEach
+  data.forEach((pokemon) => {
+    // creamos un template(string) por cada elemento del array
+    const card = `
+      <div class="card-link">
+        <article class="blog-card">
+          <img class="post-image" src="${ pokemon.img }" />
+          <h3 class="post-title">${ pokemon.id }</h3>
+          <div class="article-details">
+            <h4 class="post-category">${ pokemon.name }</h4>
+          </div>
+        </article>
+      </div>
+    `;
+    templateListOfCards += card;
+  })
+  return templateListOfCards
+};
 
 
 /*let list = POKEMON.pokemon;
@@ -25,6 +51,8 @@ for (var i = 0; i < arrpokemon.length; i++) {
  console.log(arrpokemon[i].name);
 }
 */
+
+
 
 
 
---
+++ src/main.js
@@ -0,0 +1,5 @@
+document.getElementById("list-pokemones").innerHTML = pokemones;
+
+//Mostrando todos los pokemones.
+const ListPokemones = document.getElementById("list-pokemones");
+ListPokemones.innerHTML = crearTemplateDeCard(listpokemon);
