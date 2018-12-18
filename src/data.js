b77270329b333ef1d18131151d00b7f9d94541b3
Tu-username <tucorreo@gmail.com>
2018-12-17 22:09:02 -0500

    funcion Mostrar data en el HTML

--- src/data.js
+++ src/data.js
@@ -1,10 +1,30 @@
-let apokemon = Object.values(POKEMON.pokemon )
-for (let i = 0; i < apokemon.length; i++){
+let listpokemon = POKEMON.pokemon;
+let pokemon = JSON.stringify (listpokemon);
+document.getElementById ("list-pokemones").innerHTML = pokemon;
+
+
+
+/*let listpokemon = POKEMON.pokemon
+for (let i = 0; i < listpokemon.length; i++) {
+ console.log(listpokemon[i].id);
+}
+
+document.getElementById("list-pokemones").innerHTML = listpokemon;*/
+
+
+
+
+
+/*let list = POKEMON.pokemon;
+for (let i = 0; i < list.length; i++){
   const abc= apokemon [i]
 console.log (abc.name);}
 
-
-
+var arrpokemon = POKEMON.pokemon
+for (var i = 0; i < arrpokemon.length; i++) {
+ console.log(arrpokemon[i].name);
+}
+*/
 
 
 
