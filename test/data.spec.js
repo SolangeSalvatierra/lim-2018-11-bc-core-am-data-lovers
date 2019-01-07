6a9713124e1bf214a64f5e4be41dc2fefbc15df1
SolangeCorySalvatierraQuispe <solangraf@gmail.com>
2019-01-07 16:01:54 -0500

    test de ordenado y filtrado corregido

--- test/data.spec.js
+++ test/data.spec.js
@@ -25,6 +25,24 @@ const output2 = [
   {id: '1', num: '001', name: 'Bulbasaur', type: [ 'Grass', 'Poison' ]},
 ];
 
+const output5 = 
+[
+  {id: '1', num: '001', name: 'Bulbasaur', type: [ 'Grass', 'Poison' ]},
+  {id: '4', num: '004', name: 'Charmander', type: ['Fire' ]},
+  {id: '5', num: '005', name: 'Charmeleon', type: [ 'Fire' ]},
+  {id: '2', num: '002', name: 'Ivysaur', type: [ 'Grass', 'Poison' ]},
+  {id: '3', num: '003', name: 'Venusaur', type: [ 'Grass', 'Poison' ]},
+];
+
+const output4 = 
+[
+  {id: '1', num: '001', name: 'Bulbasaur', type: [ 'Grass', 'Poison' ]},
+  {id: '2', num: '002', name: 'Ivysaur', type: [ 'Grass', 'Poison' ]},
+  {id: '3', num: '003', name: 'Venusaur', type: [ 'Grass', 'Poison' ]},
+  {id: '4', num: '004', name: 'Charmander', type: ['Fire' ]},
+  {id: '5', num: '005', name: 'Charmeleon', type: [ 'Fire' ]},
+];
+
 const output3 = [
   {id: '3', num: '003', name: 'Venusaur', type: [ 'Grass', 'Poison' ]},
   {id: '2', num: '002', name: 'Ivysaur', type: [ 'Grass', 'Poison' ]},
@@ -43,11 +61,17 @@ describe('POKE', () => {
     it('deberia retornar un array ordenando alfabeticamente', () => {
       expect(POKE.sortData(input, 'name', 0)).toEqual(output);
     });
-    // que el usuario pueda Ordenar Ascendente y Descendente
-    it('deberia ser una funcion', () => {
-      expect(typeof POKE.sortData).toBe('function');
+    it('deberia retornar un array ordenando alfabeticamente', () => {
+      expect(POKE.sortData(input, 'name', 'A-Z')).toEqual(output5);
     });
+    /* it('deberia retornar un array ordenando alfabeticamente', () => {
+      expect(POKE.sortData(input, 'name', 'A-Z')).toEqual(output5);
+    }); */
+    // que el usuario pueda Ordenar Ascendente y Descendente
     it('deberia retornar un array ordenando ascendentemente', () => {
+      expect(POKE.sortData(input, 'number', 'asc')).toEqual(output4);
+    });
+    it('deberia retornar un array ordenando descendente', () => {
       expect(POKE.sortData(input, 'number', 0)).toEqual(output2);
     });
   });
