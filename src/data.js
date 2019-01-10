81d254a2d9e373ae4f361bba8bc75014fedce5c9
kbaham <kyarabahamonde09@gmail.com>
2019-01-10 12:49:17 -0500

    Maquetando la parte del calculo

--- src/data.js
+++ src/data.js
@@ -1,27 +1,89 @@
+/* let listpokemon = POKEMON.pokemon;
+let pokemon = JSON.stringify (listpokemon);
+document.getElementById ("list-pokemones").innerHTML = pokemon; */
+
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 
-//const example = () => {
-  //return 'example';
-//};
+const sortData = (data, sortBy, sortOrder) => {
+  let arrOrder = [];
+  /* ordenado alfabeticamente */
+  if (sortBy === 'name') {
+    if (sortOrder === 'A-Z') {
+      arrOrder = data.sort(function(abc, bcd) {
+        if (abc.name > bcd.name) return 1;
+        if (abc.name === bcd.name) return 0;
+        return -1;
+      });
+    } else {
+      arrOrder = data.sort(function(abc, bcd) {
+        if (bcd.name > abc.name) return 1;
+        if (bcd.name === abc.name) return 0;
+        return -1;
+      });
+    }
 
-//window.example = example;
+  /* ordenado por id */
+  } else if (sortBy === 'number') {
+    if (sortOrder === 'asc') {
+      arrOrder = data.sort(function(abc, bcd) {
+        if (abc.id > bcd.id) return 1;
+        if (abc.id === bcd.id) return 0;
+        return -1;
+      });
+    } else {
+      arrOrder = data.sort(function(abc, bcd) {
+        if (bcd.id > abc.id) return 1;
+        if (bcd.id === abc.id) return 0;
+        return -1;
+      });
+    }
+  } 
+  return arrOrder;
+};
 
-const listPokemon = POKEMON.pokemon;
-const filterData = (data, condition) =>{
-  	let arrTipo = [];
-	for (let i = 0; i < data.length; i++){
-	for(let x =0; x < data[i].type.length; x++)
-	if(data[i].type[x] === condition){
-   arrTipo.push(data[i] , data[i].name , data[i].id)
-   
+const filterData = (data, condition) => {
+  let arrType = [];
+  for (let i = 0; i < data.length; i++) {
+    for (let x = 0; x < data[i].type.length; x++) {
+      if (data[i].type[x] === condition) {
+        arrType.push(data[i]);
+      }
+    }  
   }
- }
- return arrTipo;
+  return arrType;
+};
+
+/* const computeStats = (data, typePoke) => {
+  let tipoPorcen = [];
+  for (let i = 0; i < data.length; i++) {
+    for (let x = 0; x < data[i].type.length; x++) {
+      if (data[i].type[x] === typePoke) {
+        tipoPorcen.push(data[i] / 151 * 100).toFixed(2);
+      }
+    }
+  }
+  return (tipoPorcen + '%');
+};*/
+
+/* const computeStats = (data, typePoke) => {
+let tipoPorcen = [];
+  for (let i = 0; i < data.length; i++) {
+    for (let x = 0; x < data[i].type.length; x++) {
+      if(data[i].type[x] === typePoke){
+  tipoPorcen.push(data[i].type)
 }
+var cantPoke = ((tipoPorcen.length)/151*100).toFixed(2);
 
-//funcion que pinta
-//primero priorizar, hacerlo junta
+}
+return(cantPoke + "%")
+}
+console.log(computeStats(data,"Poison")) */
 
 
 
+window.POKE = {
+  sortData,
+  filterData,
+  /*computeStats,*/
+};
--- src/main.js
+++ src/main.js
@@ -1,35 +1,138 @@
-/*const almuerzo = [
- {principal: 'arepa', postre: 'helado'},
- {principal: 'taco', postre: 'torta de queso'},
- {principal: 'pizza', postre: 'galletas'},
- {principal: 'sushi', postre: 'quesillo'},
-];
+const dataPoke = POKEMON.pokemon;
+// Mostrando con template
+const template = (list) => {
+  let templateListPoke = '';
+  // recorremos nuestro array con forEach
+  list.forEach((dataPoke) => {
+    // creamos un template(string) por cada elemento del array
+    const card = `
+      <div class='card-link'>
+        <article class='blog-card'>
+          <img class='post-image' src='${dataPoke.img}' />
+          <h3 class='post-id'>${dataPoke.id}</h3>
+          <div class='article-details'>
+            <h4 class='post-title'>${dataPoke.name}</h4>
+            <h6 class='post-category'>${dataPoke.type}</h6>
+          </div>
+        </article>
+      </div>
+    `;
+    templateListPoke += card;
+  });
+  document.getElementById('allThePokemones').innerHTML = templateListPoke;
+};
+template(dataPoke);
 
-const platosPrincipales = [];
-for (let i = 0; i < almuerzo.length; i++) {
-platosPrincipales.push(almuerzo[i].principal);
-    
-}
-console.log(platosPrincipales);*/
-/*const containerType = document.getElementById("contType");
-containerType.addEventListener('click', (e) => {
- containerList.innerHTML = templateCard(filterData(arrPokemon, e.target.getAttribute("value")));
+// Mostrar Funcion Sort con Select 
+document.getElementById('orderpokemon').addEventListener('change', (evt) => {
+  const selectedIndex = evt.currentTarget.selectedIndex;
+  // sortBy.options[sortBy.selectedIndex].value;
+  if (selectedIndex === 3) {
+    template(POKE.sortData(dataPoke, 'name', 'A-Z'));
+  } else if (selectedIndex === 4) {
+    template(POKE.sortData(dataPoke, 'name', 'Z-A'));
+  } else if (selectedIndex === 2) {
+    template(POKE.sortData(dataPoke, 'number', 'asc'));
+  } else if (selectedIndex === 1) {
+    template(POKE.sortData(dataPoke, 'number', 'des'));
+  } 
 });
 
-var listPokemon = POKEMON.pokemon ;
-var typePoison = [];
-for (let i = 0; i < listPokemon.length; i++){
- for(let x =0; x < listPokemon[i].type.length; x++)
- if(listPokemon[i].type[x] === 'Water'){
-   typePoison.push(listPokemon[i].type)
- }
-
-}
-console.log(typePoison);*/
-
-const contenedor = document.getElementById("lista-por-tipo");
-const listaTipo = document.getElementById("listTipoPoke");
-listaTipo.addEventListener("onchange", (e) =>{
-
-	contenedor.innerHTML = filterData(listPokemon, e.target.getAttribute("value"));
+// Mostrar Funcion Filter con Select 
+document.getElementById('typepokemon').addEventListener('change', (evt) => {
+  const selectedIndex = evt.currentTarget.selectedIndex;
+  if (selectedIndex === 1) {
+    template(POKE.filterData(dataPoke, 'Water'));
+  } else if (selectedIndex === 2) {
+    template(POKE.filterData(dataPoke, 'Bug'));
+  } else if (selectedIndex === 3) {
+    template(POKE.filterData(dataPoke, 'Dragon'));
+  } else if (selectedIndex === 4) {
+    template(POKE.filterData(dataPoke, 'Electric'));
+  } else if (selectedIndex === 5) {
+    template(POKE.filterData(dataPoke, 'Ghost'));
+  } else if (selectedIndex === 6) {
+    template(POKE.filterData(dataPoke, 'Fire'));
+  } else if (selectedIndex === 7) {
+    template(POKE.filterData(dataPoke, 'Ice'));
+  } else if (selectedIndex === 8) {
+    template(POKE.filterData(dataPoke, 'Fighting'));
+  } else if (selectedIndex === 9) {
+    template(POKE.filterData(dataPoke, 'Normal'));
+  } else if (selectedIndex === 10) {
+    template(POKE.filterData(dataPoke, 'Grass'));
+  } else if (selectedIndex === 11) {
+    template(POKE.filterData(dataPoke, 'Psychic'));
+  } else if (selectedIndex === 12) {
+    template(POKE.filterData(dataPoke, 'Rock'));
+  } else if (selectedIndex === 13) {
+    template(POKE.filterData(dataPoke, 'Ground'));
+  } else if (selectedIndex === 14) {
+    template(POKE.filterData(dataPoke, 'Poison'));
+  } else if (selectedIndex === 15) {
+    template(POKE.filterData(dataPoke, 'Flying'));
+  } 
 });
+
+
+// Nav Inicio
+document.getElementById('Start').addEventListener('click', () => {
+  document.getElementById('home').style.display = 'block';
+  document.getElementById('listdePokemones').style.display = 'none';
+  document.getElementById('listCalcuPoke').style.display = 'none'; 
+});
+
+// Nav Order
+document.getElementById('Order').addEventListener('click', () => {
+  document.getElementById('listdePokemones').style.display = 'block';
+  document.getElementById('orderpokemon').style.display = 'block';
+  document.getElementById('home').style.display = 'none';
+  document.getElementById('typepokemon').style.display = 'none';
+  document.getElementById('listCalcuPoke').style.display = 'none'; 
+});
+
+// Nav Filter
+document.getElementById('Filter').addEventListener('click', () => {
+  document.getElementById('listdePokemones').style.display = 'block';
+  document.getElementById('typepokemon').style.display = 'block';
+  document.getElementById('home').style.display = 'none';
+  document.getElementById('orderpokemon').style.display = 'none';
+  document.getElementById('listCalcuPoke').style.display = 'none'; 
+});
+
+// Nav Calculate
+document.getElementById('Calculate').addEventListener('click', () => {
+  document.getElementById('listCalcuPoke').style.display = 'block';
+  document.getElementById('home').style.display = 'none';
+  document.getElementById('listdePokemones').style.display = 'none';
+});
+
+// botton informacion sobre Pokemon Go
+document.getElementById('mostrar-inf').addEventListener('click', () => {
+  document.getElementById('information').style.display = 'block';
+});
+
+// botton Conoce tus Pokemones
+document.getElementById('pokemones').addEventListener('click', () => {  
+  document.getElementById('listdePokemones').style.display = 'block';
+  document.getElementById('home').style.display = 'none';
+  document.getElementById('typepokemon').style.display = 'none';
+  document.getElementById('listCalcuPoke').style.display = 'none'; 
+});
+
+
+
+let myIndex = 0;
+carousel();
+
+function carousel() {
+  let i;
+  let x = document.getElementsByClassName('mySlides');
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = 'none';  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}   
+  x[myIndex - 1].style.display = 'block' ;  
+  setTimeout(carousel, 2000); // cambio a 5 segundos
+}
